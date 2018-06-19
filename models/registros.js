b12e294d8744470239a095451798062af06f52e2
WillyBaldeon <alex.memito@hotmail.com>
2018-06-19 18:40:05 -0500

    Agregué las librerías de JQuery y JQueryUI. También, modifiqué el modelo de la sección de publicaciones en registros.js.

--- models/registros.js
+++ models/registros.js
@@ -30,7 +30,7 @@ const usuariosModel = mongoose.model('usuarios',usuariosSchema);
 
 module.exports = {
   create: (req,res,next)=>{
-    const usuario = new usuariossModel({
+    const usuario = new usuariosModel({
       _id: new mongoose.Types.ObjectId(),
       usuario: req.body.usuario,
       contraseña: req.body.contraseña,
@@ -38,7 +38,7 @@ module.exports = {
       apellidos: req.body.apellidos,
       telefono: req.body.telefono,
       correo: req.body.telefono,
-      valoracion: req.body.valoracion,
+      valoracion: req.body.valoracion
     });
     usuario
       .save()
@@ -126,6 +126,121 @@ module.exports = {
       .then(result => {
         res.status(200).json({
           message: 'Usuario eliminado'
+        });
+      })
+      .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  }
+};
+
+const publicacionesModel = mongoose.model('publicaciones',publicacionesSchema);
+
+module.exports = {
+  create: (req,res,next)=>{
+    const publicacion = new publicacionesModel({
+      _id: new mongoose.Types.ObjectId(),
+      placa: req.body.placa,
+      estado: req.body.estado,
+      costo: req.body.costo,
+      modelo: req.body.modelo,
+      marca: req.body.marca,
+      año_veh: req.body.año_veh,
+      titulo: req.body.titulo,
+      descripcion: req.body.descripcion,
+      fecha_pub: req.body.fecha_pub,
+      imagenes: req.body.imagenes,
+      tipo_servicio: req.body.tipo_servicio
+    });
+    publicacion
+      .save()
+      .then(result =>{
+        res.status(200).json({
+          message: 'Publicación Creada con Exito',
+          data:{
+            ...result
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error:err
+        });
+      });
+  },
+  find: (req, res, next) => {
+    publicacionesModel.find()
+      .select('_id placa estado costo modelo marca año_veh titulo descripcion fecha_pub imagenes tipo_servicio')
+      .exec()
+      .then(docs => {
+        const response = {
+          count: docs.length,
+          data : docs.map(doc=>{
+            return{
+                ...doc
+            };
+          })
+        };
+        res.status(200).json(response);
+      })
+      .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+          error:err
+        });
+      });
+  },
+  update: (req, res, next) =>{
+    const id = req.params.id;
+    let updateParams = {
+      ...req.body
+    };
+    publicacionesModel.update({_id: id}, {$set: updateParams})
+      .exec()
+      .then(result =>{
+        res.status(200).json({
+          message: 'Publicación Actualizada'
+        });
+      })
+      .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  },
+  findOne: (req,res,next)=>{
+    const id = req.params.id;
+    publicacionesModel.findById(id)
+      .select('_id placa estado costo modelo marca año_veh titulo descripcion fecha_pub imagenes tipo_servicio')
+      .exec()
+      .then(doc => {
+        if(doc){
+          res.status(200).json({
+            data:doc,
+          });
+        } else {
+          res.status(404).json({message: 'No valid entry found for provided ID'});
+        }
+      })
+      .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  },
+  delete: (req, res, next)=>{
+    const id = req.params.id;
+    publicacionesModel.remove({_id: id})
+      .exec()
+      .then(result => {
+        res.status(200).json({
+          message: 'Publicación eliminada'
         });
       })
       .catch(err =>{
