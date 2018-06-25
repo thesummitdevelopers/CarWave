c59efd60203b87810149683fa47be64dcbff8963
OscarMolleapaza <molleapazafloreso@gmail.com>
2018-06-25 10:30:17 -0500

    Adjunto el script de Login

---
+++ scripts/login.js
@@ -0,0 +1,16 @@
+$('document').ready(function(){
+  alert("sagsag");
+   $('#ingresar').click(function(){
+     var usuario = $("#user").val();
+     var password = $("#passwd").val();
+
+
+     if ( usuario = "" || password = ""){
+       alert("Ingrese los campos");
+     }
+     else {
+       alert("Safgsdg");
+     }
+
+   });
+});
